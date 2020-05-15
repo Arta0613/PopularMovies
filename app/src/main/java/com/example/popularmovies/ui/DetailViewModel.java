80ed495106a084a35646dfbcba21d8aeb5b5fb7b
Mohammed Amin <arta0613@gmail.com>
2020-05-15 03:36:24 -0400

    add movie reviews to detail screen

--- app/src/main/java/com/example/popularmovies/ui/DetailViewModel.java
+++ app/src/main/java/com/example/popularmovies/ui/DetailViewModel.java
@@ -7,8 +7,10 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.popularmovies.data.MovieApiUseCases;
+import com.example.popularmovies.domain.moviereviews.MovieReviewItem;
 import com.example.popularmovies.domain.movies.MovieItem;
 import com.example.popularmovies.domain.movietrailers.MovieTrailerItem;
+import com.example.popularmovies.ui.adapters.MovieReviewAdapter;
 import com.example.popularmovies.ui.adapters.MovieTrailerAdapter;
 
 import java.util.ArrayList;
@@ -24,9 +26,11 @@ public class DetailViewModel extends ViewModel {
 
     private final MovieApiUseCases movieApiUseCases = new MovieApiUseCases();
     private final MovieTrailerAdapter trailerAdapter = new MovieTrailerAdapter();
+    private final MovieReviewAdapter reviewAdapter = new MovieReviewAdapter();
 
     private final CompositeDisposable disposables = new CompositeDisposable();
     private List<MovieTrailerItem> movieTrailerItems;
+    private List<MovieReviewItem> movieReviewItems;
 
     public final MutableLiveData<Boolean> loadingIndicator = new MutableLiveData<>(false);
     public final MutableLiveData<Boolean> errorView = new MutableLiveData<>(false);
@@ -41,21 +45,27 @@ public class DetailViewModel extends ViewModel {
         return trailerAdapter;
     }
 
+    public MovieReviewAdapter getReviewAdapter() {
+        return reviewAdapter;
+    }
+
     public void setItemClickListener(final ItemClickListener itemClickListener) {
         trailerAdapter.setItemClickListener(itemClickListener);
+        reviewAdapter.setItemClickListener(itemClickListener);
     }
 
     public void setMovieTrailerItems(final List<MovieTrailerItem> movieTrailerItems) {
         this.movieTrailerItems = movieTrailerItems != null ? movieTrailerItems : new ArrayList<>();
     }
 
+    public void setMovieReviewItems(final List<MovieReviewItem> movieReviewItems) {
+        this.movieReviewItems = movieReviewItems != null ? movieReviewItems : new ArrayList<>();
+    }
+
     public void loadData() {
         setLoadingIndicator(true);
         getMovieTrailers();
-    }
-
-    public void retryLoadingExtras() {
-        getMovieTrailers();
+        getMovieReviews();
     }
 
     @Override
@@ -85,6 +95,27 @@ public class DetailViewModel extends ViewModel {
                 }));
     }
 
+    private void getMovieReviews() {
+        disposables.add(movieApiUseCases.getMovieReviews(movieItem.getMovieId())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<MovieReviewItem>>() {
+                    @Override
+                    public void onSuccess(@NonNull final List<MovieReviewItem> movieReviewItems) {
+                        Log.d(LOG, "Success loading reviews");
+                        setAndLoadReviews(movieReviewItems);
+                        setLoadingIndicator(false);
+                        setErrorView(false);
+                    }
+
+                    @Override
+                    public void onError(@NonNull final Throwable e) {
+                        Log.e(LOG, "Error occurred while loading reviews", e);
+                        setLoadingIndicator(false);
+                        setErrorView(true);
+                    }
+                }));
+    }
+
     private void setLoadingIndicator(final boolean visibility) {
         loadingIndicator.setValue(visibility);
     }
@@ -97,5 +128,11 @@ public class DetailViewModel extends ViewModel {
         setMovieTrailerItems(movieTrailers);
         trailerAdapter.setMovieTrailers(movieTrailers);
         trailerAdapter.notifyDataSetChanged();
+    }
+
+    private void setAndLoadReviews(final List<MovieReviewItem> movieReviews) {
+        setMovieReviewItems(movieReviews);
+        reviewAdapter.setMovieReviews(movieReviews);
+        reviewAdapter.notifyDataSetChanged();
     }
 }
